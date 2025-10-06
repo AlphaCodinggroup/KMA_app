70d55ab0b1e471c511f240a1d3626c818a05ae5e
Franco Ghilardi <franco.ghilardi1@gmail.com>
2025-10-06 10:19:24 -0300

    feat: sea agrega imagenes a los steps con zoom

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
+import 'react-native-gesture-handler'
 import { Stack } from 'expo-router'
 import { AuthGuard } from '@processes/auth-guard'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const RootLayout: React.FC = () => {
   return (
-    <AuthGuard>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(app)" />
-      </Stack>
-    </AuthGuard>
+    <GestureHandlerRootView>
+      <AuthGuard>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(app)" />
+        </Stack>
+      </AuthGuard>
+    </GestureHandlerRootView>
   )
 }
 
